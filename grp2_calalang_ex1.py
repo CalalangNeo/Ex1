0e4a0859f040a78e0bcf9387a3b9373fe221fc0c
CalalangNeo <143771557+CalalangNeo@users.noreply.github.com>
2024-09-08 15:31:44 +0800

    grp2_calalang_ex1.py

---
+++ grp2_calalang_ex1.py
@@ -0,0 +1,37 @@
+print(“Hello, I’m Neo Calalang! BSCS3G”)
+
+#Python comment 
+
+#My first simple script
+
+#Python variables
+
+number = 2
+name = “Nea”
+
+print(“Name:” , name)
+print(“Number: , number)
+
+#Python variable names
+
+argame = “Anne”
+bandalan = “Julianna”
+bayani = “kitt”
+group = “groupmates”
+
+print(argame)
+print(bandalan)
+print(bayani)
+print(groupmates)
+
+#Many Values to Multiple Variables
+#a.Assign values to multiple variables in one line
+
+x, y, z = 10, 20, 30
+print("x:", x, "y:", y, "z:", z)
+
+#One Values to Multiple Variables
+#a. Assign the same value to multiple variables in one line.
+
+a = b = c = "500"
+print("a:", a, "b:", b, "c:", c)
